3af0814b7446034183798730908d0a645add959e
Yushko, Dzmitry <dyushko@scnsoft.com>
2020-03-02 17:29:23 +0300

    Refactored code in render.py, now instead of use Pil Image class render use numpy arrays

--- utils/render.py
+++ utils/render.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass, field
 from typing import Tuple, List, Optional
 
 import glm
-from PIL import Image, ImageDraw, ImageColor
+import numpy as np
+from PIL import ImageDraw, ImageColor
 
 from utils.gridmap import GridMap
 from utils.hit import Hit
@@ -23,12 +24,12 @@ class Render:
     background: Tuple[int, ...] = COLOR_BLACK
     help_map_size: glm.ivec2 = glm.ivec2(200, 200)
 
-    _buffer: Image.Image = field(init=False)
-    _back_buffer: Image.Image = field(init=False)
+    _buffer: np.ndarray = field(init=False)
+    _back_buffer: np.ndarray = field(init=False)
 
     def __post_init__(self):
-        self._buffer = Image.new('RGB', (self.width, self.height), self.background)
-        self._back_buffer = Image.new('RGB', (self.width, self.height), self.background)
+        self._buffer = np.zeros((self.width, self.height, 3), dtype=np.uint8)
+        self._back_buffer = np.zeros((self.width, self.height, 3), dtype=np.uint8)
 
     def swap_buffers(self):
         self._buffer, self._back_buffer = self._back_buffer, self._buffer
@@ -39,60 +40,54 @@ class Render:
         draw.rectangle(box, self.background)
 
     def draw_help_map(self,
-                      draw: ImageDraw.ImageDraw,
                       ray_origin: glm.vec2,
                       grid: GridMap,
                       scale: glm.ivec2,
                       hits: List[Hit]):
-        self.clear_buffer(draw, (0, 0, scale.x, scale.y))
         scale = scale / grid.size
         for x in range(grid.size.x):
             for y in range(grid.size.y):
                 xx, yy = x * scale.x, y * scale.y
-                value = grid.get(x, y)
-                if value == ord(' '):
-                    draw.rectangle((xx, yy, xx + scale.x, yy + scale.y), outline=COLOR_GREEN)
-                else:
-                    color = COLOR_RED
-                    if value < len(grid.texture_atlas):
-                        texture = grid.texture_atlas[value]
-                        color = tuple(texture.data[0, 0])
-                    draw.rectangle((xx, yy, xx + scale.x, yy + scale.y), fill=color)
+                value = chr(grid.get(x, y))
+                if value == ' ':
+                    continue
+                self._back_buffer[yy:yy + scale.y, xx:xx + scale.x] = COLOR_RED
 
-        player = ray_origin / grid.cell_size * scale
-        draw.rectangle((*(player - 1), *(player + 1)), COLOR_WHITE)
-        for hit in hits:
-            point = hit.point / grid.cell_size * scale
-            draw.line((*player, *point), COLOR_YELLOW)
+        player = glm.ivec2(glm.floor(ray_origin / grid.cell_size * scale))
+        self._back_buffer[player.y - 1:player.y + 1, player.x - 1:player.x + 1] = COLOR_WHITE
+        # for hit in hits:
+        #     point = hit.point / grid.cell_size * scale
+        #     draw.line((*player, *point), COLOR_YELLOW)
 
     def draw_scene(self,
-                   draw: ImageDraw.ImageDraw,
                    player_radians: float,
                    projection_plane: float,
                    wall_height: int,
-                   hits: List[Hit]):
+                   hits: List[Hit], grid: GridMap):
+        min_val = 0
         y = self.height / 2
+        max_val = self.height - 1
         precomputed = wall_height * projection_plane
         for x, hit in enumerate(hits):
             correct_distance = hit.length * glm.cos(hit.radians - player_radians)
             column_height = precomputed / correct_distance
-            draw.line((x, y - column_height, x, y + column_height), COLOR_GRAY)
-
-    @property
-    def draw(self):
-        return ImageDraw.Draw(self._back_buffer)
+            h1, h2 = glm.ivec2(glm.floor(glm.clamp(glm.vec2(y - column_height, y + column_height), min_val, max_val)))
+            self._back_buffer[h1:h2, x] = COLOR_GRAY
 
     def update_state(self, ray: Ray, fov: float, grid: GridMap):
-        draw = self.draw
-        self.clear_buffer(draw)
+        self._back_buffer[:] = self.background
 
         hits = grid.ray_casting(self.width, fov, ray)
 
-        self.draw_scene(draw, ray.radians, 277, grid.cell_size, hits)
-        self.draw_help_map(draw, ray.origin, grid, self.help_map_size, hits)
+        self.draw_scene(ray.radians, 277, grid.cell_size, hits, grid)
+        # self.draw_help_map(ray.origin, grid, self.help_map_size, hits)
 
         self.swap_buffers()
 
     @property
+    def size(self):
+        return self.width, self.height
+
+    @property
     def buffer(self):
-        return self._buffer
+        return self._back_buffer.swapaxes(0, 1)
